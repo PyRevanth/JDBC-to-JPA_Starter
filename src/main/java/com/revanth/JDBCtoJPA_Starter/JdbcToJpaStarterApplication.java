74d004c614e98eb7106e4cb19d711f1f82910d26
revanth <revanthchimmani@gmail.com>
2020-03-02 10:08:34 +0530

    added Starter kit(Web,JDBC,JPA,Lombok,H2)

---
+++ src/main/java/com/revanth/JDBCtoJPA_Starter/JdbcToJpaStarterApplication.java
@@ -0,0 +1,13 @@
+package com.revanth.JDBCtoJPA_Starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcToJpaStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcToJpaStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revanth/JDBCtoJPA_Starter/JdbcToJpaStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revanth.JDBCtoJPA_Starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcToJpaStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
